d89f3c35c8a850aa33d3ae30fb041835aa595eaa
Pranay K R <145147762+Pranay50x@users.noreply.github.com>
2025-01-30 21:50:26 +0530

    Update 8.c
    added structure for Cqueue

--- practice/8.c
+++ practice/8.c
@@ -3,97 +3,100 @@
  *
  *       Filename:  8.c
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
- *        Created:  09/01/2025 15:41:47
+ *        Created:  30/01/2025 13:49:37
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  YOUR NAME (),
+ *   Organization:
  *
  * =====================================================================================
  */
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX 4
+#define MAX 10
 
-int cqueue[MAX];
-int front = -1 ,rear=-1; 
+struct Cqueue{
+    int data;
+};
 
-void insert(int val){
-    
-    if((front == 0 && rear == MAX-1) || ((rear+1)%MAX == front)){
-           printf("Circular queue is full\n"); 
-           return; 
-    }
+struct Cqueue cqueue[MAX];
+int front = -1, rear=-1;
 
-    if(front==-1)
-        front = rear =0; 
+void insert(int x){
+   if((rear==MAX-1 && front ==0) || ((rear+1)%MAX==front)){
+       printf("Queue Overflow\n");
+       return ;
+   }
 
-    else if(rear == MAX -1)
-        rear =0; 
-    else
-        rear++;
-    cqueue[rear] = val;
-    printf("\nValue added is:%d\n",val); 
+   if(front==-1)
+       front = rear = 0;
+   else if(rear==MAX-1)
+       rear =0;
+   else
+       rear++;
+
+   cqueue[rear].data =x;
+   printf("%d inserted in Circu;ar queue\n",x);
 }
 
 
-int delete() {
-    if(front == -1){
-        printf("Circular Queue Underflow\n");
-        return -1; 
+int delete(){
+    if(front==-1){
+        printf("Queue Underflow\n");
+        return -1;
     }
 
-    int val = cqueue[front]; 
+    int x = cqueue[front].data;
     if(front==rear)
-        front = rear -1;
-    else if(front == MAX-1)
-        front = 0; 
-    else 
+        front = rear = -1;
+    else if(front==MAX-1)
+        front=0;
+    else
         front++;
-
-    return val;
+    return x;
 }
 
 void display(){
     if(front==-1){
-        printf("Circular queue is empty\n"); 
+        printf("Circular queue underflow\n");
         return ;
     }
 
-    printf("\nFront -> %d\nItems in Circular Queue: ", front); 
-    for(int i = front; i!=rear; i= (i+1)%MAX)
-        printf("%d ",cqueue[i]); 
+    printf("\nFront: %d\nItems in Queue: ",front);
+    for(int i = front; i!=rear;i=(i+1)%MAX)
+        printf("%d ",cqueue[i].data);
 
-    printf("%d ", cqueue[rear]); 
-    printf("\nRear -> %d", rear);    
+    printf("%d", cqueue[rear].data);
+    printf("\nRear: %d",rear);
+
 }
 
-int main(){
 
-    int ch, val,res;
+int main(){
+    int ch,x,res;
+
     while(1){
-        printf("\n1. Insert Element");
-        printf("\n2. Delete Element"); 
-        printf("\n3. Display Items");
-        printf("\n4. Exit"); 
-        printf("\nEnter your choice: "); 
+        printf("\n1. Insert ");
+        printf("\n2. Delete ");
+        printf("\n3. Display ");
+        printf("\n4. Exit ");
+        printf("\nEnter your choice: ");
         scanf("%d",&ch);
 
         switch(ch){
-            case 1: 
-                printf("\nEnter the element: "); 
-                scanf("%d",&val); 
-                insert(val);
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &x);
+                insert(x);
                 break;
-            case 2: 
-                //int res;
+            case 2:
                 res = delete();
                 if(res!=-1)
-                    printf("\nElement removed is -> %d\n",res);
+                    printf("%d removed from the queue\n",res);
                 break;
             case 3:
                 display();
@@ -101,9 +104,8 @@ int main(){
             case 4:
                 exit(0);
                 break;
-            default: 
-                printf("Enter valid choice\n");
+            default:
+                printf("ENter the valid choice");
         }
     }
-    return 0;
 }
